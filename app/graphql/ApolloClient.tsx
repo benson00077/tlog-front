ded189fcf3723fca5e55cec53bc6829f5d71def6
Benson <48788513+benson00077@users.noreply.github.com>
2023-11-07 18:48:11 +0800

    fix: error link for apollo client

--- app/graphql/ApolloClient.tsx
+++ app/graphql/ApolloClient.tsx
@@ -1,6 +1,6 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache, from } from '@apollo/client'
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
-import { serverURI } from './config'
+import { errorLink, serverURI } from './config'
 
 /**
  *  ref: https://github.com/apollographql/apollo-client-nextjs
@@ -8,12 +8,15 @@ import { serverURI } from './config'
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: serverURI,
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
-    }),
+    link: from([
+      errorLink,
+      new HttpLink({
+        // this needs to be an absolute url, as relative urls cannot be used in SSR
+        uri: serverURI,
+        // you can disable result caching here if you want to
+        // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+        // fetchOptions: { cache: "no-store" },
+      }),
+    ]),
   })
 })
