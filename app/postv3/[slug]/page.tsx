40bc9ee5240b6bf217a025f32c672e9ae8df340e
Benson <48788513+benson00077@users.noreply.github.com>
2023-11-07 22:04:45 +0800

    feat: local mdx use ymal not export module

--- app/postv3/[slug]/page.tsx
+++ app/postv3/[slug]/page.tsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import { getAllPostsMeta, getPageData } from './util'
 import dynamic from 'next/dynamic'
+import { formatDate } from '../../_utils/utils'
+import Image from 'next/image'
 
 // generate route segments
 export async function generateStaticParams() {
   const metas = await getAllPostsMeta()
   return metas.map((meta) => ({
-    slug: meta.id,
+    //TODO: 改成走ID？
+    slug: meta.fileName,
   }))
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params
-  const { meta } = await getPageData(`${slug}.mdx`)
-  const { title, date, description } = meta
+  const meta = await getPageData(`${slug}.mdx`)
+  const { title, updatedAt, description, posterURL } = meta
   const Post = dynamic(() => import(`../(mdx)/${slug}.mdx`))
 
   if (!Post) return <div>Loading...</div>
   return (
     <div key={slug}>
       <h1>{title}</h1>
-      <time>{date}</time>
+      <Image
+        alt={title}
+        src={posterURL}
+        width={900}
+        height={550}
+        style={{ objectFit: 'cover' }}
+        className="rounded-lg"
+      />
+      <time>{formatDate(updatedAt.toString())}</time>
       <blockquote>{description}</blockquote>
       <Post />
     </div>
--- app/postv3/[slug]/util.ts
+++ app/postv3/[slug]/util.ts
@@ -1,31 +1,40 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import fs from 'fs'
 import path from 'path'
+import fm from 'front-matter'
 
-export interface MetaData {
-  [key: string]: any
+interface frontMatter {
+  title: string
+  createdAt: Date
+  updatedAt: Date
+  description: string
+  posterURL: string
+  id: string
+  tags?: string[]
+  category?: string
+}
+export interface MetaData extends frontMatter {
+  fileName: string
 }
 
 const postsDirectory: string = path.join(process.cwd(), 'app/postv3/(mdx)')
 const fileNames: string[] = fs.readdirSync(postsDirectory)
 
 export async function getPageData(mdxFileName: string): Promise<MetaData> {
-  //ref: https://stackoverflow.com/a/43112861/16124226
-  const { meta } = await import(`/app/postv3/(mdx)/${mdxFileName}`)
-  const postData: MetaData = {
-    meta: { ...meta, id: mdxFileName.replace(/\.mdx/, '') },
-  }
-  return postData
+  const filePath = path.resolve('./app/postv3/(mdx)', mdxFileName)
+  const data = fs.readFileSync(filePath, 'utf-8')
+  const { attributes, body } = fm(data)
+  return { ...(attributes as frontMatter), fileName: mdxFileName.replace(/\.mdx/, '') }
 }
 
 export async function getAllPostsMeta(): Promise<MetaData[]> {
   const metas = []
   for (const file of fileNames) {
-    const { meta } = await getPageData(file)
+    const meta = (await getPageData(file)) as MetaData
     metas.push(meta)
   }
   metas.sort((a: MetaData, b: MetaData) => {
-    return a.date < b.date ? 1 : -1
+    return a.updatedAt < b.updatedAt ? 1 : -1
   })
   return metas
 }
